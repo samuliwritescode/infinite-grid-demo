e23a274b0a8b8525550af5f1100c2df47f78598d
Samuli Penttila <samuli@vaadin.com>
2023-04-05 09:49:58 +0300

    Introduce docker

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM eclipse-temurin:17 AS BUILD
+RUN apt update && apt install unzip maven -y
+COPY . /app/
+WORKDIR /app/
+RUN mvn -C clean test package -Pproduction
+WORKDIR /app/target/
+RUN ls -la
+RUN unzip *.zip -d app/
+
+FROM eclipse-temurin:17
+COPY --from=BUILD /app/target/app /app/
+WORKDIR /app/
+EXPOSE 8080
+ENTRYPOINT ./run
--- src/main/java/org/vaadin/samuli/InfiniteGridDemo.java
+++ src/main/java/org/vaadin/samuli/InfiniteGridDemo.java
@@ -1,6 +1,8 @@
 package org.vaadin.samuli;
 
+import com.github.mvysny.vaadinboot.VaadinBoot;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.notification.Notification;
@@ -13,91 +15,97 @@ import com.vaadin.flow.router.Route;
 @JsModule("./src/colorful-cell.js")
 public class InfiniteGridDemo extends VerticalLayout {
 
-  public InfiniteGridDemo() {
-    HorizontalLayout firstRow = new HorizontalLayout();
-    HorizontalLayout secondRow = new HorizontalLayout();
-    firstRow.setWidth("100%");
-    secondRow.setWidth("100%");
-    firstRow.setSpacing(false);
-    secondRow.setSpacing(false);
-    firstRow.setMargin(false);
-    secondRow.setMargin(false);
+    public InfiniteGridDemo() {
+        HorizontalLayout firstRow = new HorizontalLayout();
+        HorizontalLayout secondRow = new HorizontalLayout();
+        firstRow.setWidth("100%");
+        secondRow.setWidth("100%");
+        firstRow.setSpacing(false);
+        secondRow.setSpacing(false);
+        firstRow.setMargin(false);
+        secondRow.setMargin(false);
 
-    InfiniteGrid textGrid = createInfiniteGrid();
-    textGrid.setFrozenRows(1);
-    textGrid.setFrozenColumns(1);
-    textGrid.setHtmlGenerator((x,y)-> {
-      if (x == 0 && y == 0) {
-        return "";
-      }
+        InfiniteGrid textGrid = createInfiniteGrid();
+        textGrid.setFrozenRows(1);
+        textGrid.setFrozenColumns(1);
+        textGrid.setHtmlGenerator((x, y) -> {
+            if (x == 0 && y == 0) {
+                return "";
+            }
 
-      if (x == 0) {
-        return "row "+y;
-      }
+            if (x == 0) {
+                return "row " + y;
+            }
 
-      if (y == 0) {
-        return "col "+x;
-      }
+            if (y == 0) {
+                return "col " + x;
+            }
 
-      return String.format("%d, %d", x,y);
-    }, InfiniteGrid.HTMLRenderingHints.TEXT_ONLY);
-    firstRow.add(textGrid);
+            return String.format("%d, %d", x, y);
+        }, InfiniteGrid.HTMLRenderingHints.TEXT_ONLY);
+        firstRow.add(textGrid);
 
-    InfiniteGrid htmlGrid = createInfiniteGrid();
-    htmlGrid.setHtmlGenerator((x, y) -> {
-      if (y%2==1) {
-        return String.format("%d, %d", x,y);
-      }
+        InfiniteGrid htmlGrid = createInfiniteGrid();
+        htmlGrid.setHtmlGenerator((x, y) -> {
+            if (y % 2 == 1) {
+                return String.format("%d, %d", x, y);
+            }
 
-      return String.format("<div style='height:100%%;width:100%%;background-color: #eee'>%d, %d</div>", x, y);
-    }, InfiniteGrid.HTMLRenderingHints.NORMAL);
-    firstRow.add(htmlGrid);
+            return String.format("<div style='height:100%%;width:100%%;background-color: #eee'>%d, %d</div>", x, y);
+        }, InfiniteGrid.HTMLRenderingHints.NORMAL);
+        firstRow.add(htmlGrid);
 
-    InfiniteGrid componentGrid = createInfiniteGrid();
-    componentGrid.setComponentGenerator((x, y) ->
-        new Button(
-            String.format("%d, %d", x, y),
-            e -> Notification.show(String.format("clicked (%d, %d)", x, y))
-        ));
-    secondRow.add(componentGrid);
+        InfiniteGrid componentGrid = createInfiniteGrid();
+        componentGrid.setComponentGenerator((x, y) ->
+                new Button(
+                        String.format("%d, %d", x, y),
+                        e -> Notification.show(String.format("clicked (%d, %d)", x, y))
+                ));
+        secondRow.add(componentGrid);
 
-    InfiniteGrid colorGrid = new InfiniteGrid();
-    colorGrid.setCellSize(70,70);
-    colorGrid.setItemCount(1000, 1000);
-    colorGrid.setTemplateGenerator("<colorful-cell></colorful-cell>");
-    colorGrid.getElement()
-        .addEventListener("clickcell", e -> Notification.show("Clicked " +
-            (int)e.getEventData().getObject("event.detail").getNumber("x") + ", " +
-            (int)e.getEventData().getObject("event.detail").getNumber("y")))
-        .addEventData("event.detail");
-    secondRow.add(colorGrid);
+        InfiniteGrid colorGrid = new InfiniteGrid();
+        colorGrid.setCellSize(70, 70);
+        colorGrid.setItemCount(1000, 1000);
+        colorGrid.setTemplateGenerator("<colorful-cell></colorful-cell>");
+        colorGrid.getElement()
+                .addEventListener("clickcell", e -> Notification.show("Clicked " +
+                        (int) e.getEventData().getObject("event.detail").getNumber("x") + ", " +
+                        (int) e.getEventData().getObject("event.detail").getNumber("y")))
+                .addEventData("event.detail");
+        secondRow.add(colorGrid);
 
-    firstRow.setFlexGrow(0.5, textGrid);
-    firstRow.setFlexGrow(0.5, htmlGrid);
-    secondRow.setFlexGrow(0.5, componentGrid);
-    secondRow.setFlexGrow(0.5, colorGrid);
-    setSizeFull();
-    setSpacing(true);
-    setMargin(false);
-    setPadding(false);
-    H3 title = new H3(
-        "Below there are 4 InfiniteGrids. 1. Server generated text with column and row header. 2. Server generated html. 3. Vaadin components. 4. Static lit template."
-    );
-    add(
-        title,
-        firstRow,
-        secondRow
-    );
-    setFlexGrow(0, title);
-    setFlexGrow(0.5, firstRow);
-    setFlexGrow(0.5, secondRow);
-  }
+        firstRow.setFlexGrow(0.5, textGrid);
+        firstRow.setFlexGrow(0.5, htmlGrid);
+        secondRow.setFlexGrow(0.5, componentGrid);
+        secondRow.setFlexGrow(0.5, colorGrid);
+        setSizeFull();
+        setSpacing(true);
+        setMargin(false);
+        setPadding(false);
+        H3 title = new H3(
+                "Below there are 4 InfiniteGrids. 1. Server generated text with column and row header. 2. Server generated html. 3. Vaadin components. 4. Static lit template."
+        );
+        add(
+                title,
+                new ComboBox<>(),
+                firstRow,
+                secondRow
+        );
+        setFlexGrow(0, title);
+        setFlexGrow(0.5, firstRow);
+        setFlexGrow(0.5, secondRow);
+    }
 
-  private InfiniteGrid createInfiniteGrid() {
-    InfiniteGrid infiniteGrid = new InfiniteGrid();
-    infiniteGrid.getElement().getClassList().add("borders");
-    infiniteGrid.setCellSize(200, 40);
-    infiniteGrid.setItemCount(100, 100);
-    return infiniteGrid;
-  }
+    private InfiniteGrid createInfiniteGrid() {
+        InfiniteGrid infiniteGrid = new InfiniteGrid();
+        infiniteGrid.getElement().getClassList().add("borders");
+        infiniteGrid.setCellSize(200, 40);
+        infiniteGrid.setItemCount(100, 100);
+        return infiniteGrid;
+    }
+
+    public static void main(String[] args) throws Exception {
+        //Thanks @mvysny
+        new VaadinBoot().withArgs(args).run();
+    }
 }
